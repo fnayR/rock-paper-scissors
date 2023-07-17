659b24cab769e6fa16a2c5b2b86753d165286e7d
fnayR <ryanfaturrahman@gmail.com>
2023-07-17 22:37:46 +0700

    Add playRound function to script.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,35 @@
-let computerChoice;
+let computerSelection;
+let playerSelection = prompt("Rock, paper, or scissors?", "");
 
 function getComputerChoice() {
     let choice = Math.random() * 3;
     if (choice <= 1) {
-        choice = "Rock";
+        choice = "rock";
     } else if (choice <= 2) {
-        choice = "Paper";
+        choice = "paper";
     } else {
-        choice = "Scissors";
+        choice = "scissors";
     }
-    return computerChoice = choice;
+    return computerSelection = choice;
 }
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return alert("It's a tie!");
+    } else if 
+        ((playerSelection.toLowerCase() === "rock" && computerSelection === "paper") ||
+        (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") ||
+        (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock")){
+        return alert("You lose, " + computerSelection + " beats " + playerSelection.toLowerCase() + ".")
+    } else if
+        ((playerSelection.toLowerCase() === "paper" && computerSelection === "rock") ||
+        (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") ||
+        (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors")){
+        return alert("You win, " + playerSelection.toLowerCase() + " beats " + computerSelection + ".")
+    } else {
+        return alert("Wrong choice!\nChoose between rock, paper, and scissors!")
+    }
+}
+
+console.log(getComputerChoice());
+console.log(playRound(playerSelection, computerSelection));
